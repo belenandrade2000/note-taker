a62e08ebe8ca0bb243cf8c63c67294787dfcb42a
Belen Andrade <belenandrade2000@gmail.com>
2023-04-12 15:05:15 -0700

    added comments abot code functionality

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
+
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-// Helper method for generating unique ids
 const uuid = require('./helpers/uuid');
 
 const PORT = 3001;
 
 const app = express();
 
+//middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static('public'));
 
 app.get('/', (req, res) =>
@@ -21,21 +21,15 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
  
-// GET request for reviews
-app.get('/api/notes', (req, res) => {
-  // Send a message to the client
-  fs.readFile('./db/notes.json',(err,data) => res.json(JSON.parse(data)));
 
-  // Log our request to the terminal
+app.get('/api/notes', (req, res) => {
+  fs.readFile('./db/notes.json',(err,data) => res.json(JSON.parse(data)));
   console.info(`${req.method} request received to get notes`);
 });
 
-// POST request to add a review
 app.post('/api/notes', (req, res) => {
-  // Log that a POST request was received
   console.info(`${req.method} request received to add a note`);
 
-  // Destructuring assignment for the items in req.body
   const {title, text} = req.body;
 
   // If all the required properties are present
